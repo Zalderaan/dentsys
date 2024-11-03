18d4ce935db5587af2053639d5f6c6286b85816f
Godfrey Eclarinal <godfreyeclarinal21@gmail.com>
2024-11-03 22:36:09 +0800

    repo clean done, add other front-end progress pls ty

---
+++ dentsys_client/lib/screens/login/app_colors.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color backColor = Color.fromARGB(255, 225, 225, 225);
+  static const Color mainBrownColor = Color(0xff8B4513);
+  static const Color darkBrownColor = Color(0xff5D4037);
+  static const Color textColor = Color(0xff53587A);
+  static const Color greyColor = Color(0xffAAAAAA);
+  static const Color whiteColor = Color(0xffFFFFFF);
+  static const Color goldColor = Color(0xFFE2AD5E);
+}
