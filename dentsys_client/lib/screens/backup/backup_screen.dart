ed7d4ef91c56800bd8aba000df11c7034ef2b61f
Eriiixxx <202211048@gordoncollege.edu.ph>
2024-12-07 15:28:14 +0800

    add button to export as CSV

--- dentsys_client/lib/screens/backup/backup_screen.dart
+++ dentsys_client/lib/screens/backup/backup_screen.dart
@@ -126,7 +126,7 @@ class _BackupScreenState extends State<BackupScreen> {
                                     }
                                   },
                                   icon: const Icon(
-                                    Icons.backup,
+                                    Icons.restore,
                                     color: Colors.white,
                                   ),
                                   label: const Text(
@@ -177,6 +177,40 @@ class _BackupScreenState extends State<BackupScreen> {
                                   ),
                                 ),
                               ),
+                            
+                              const SizedBox(width: 10.0),
+                              Container(
+                                  decoration: BoxDecoration(
+                                    gradient: const LinearGradient(
+                                      colors: [Color.fromARGB(255, 94, 138, 226), Color.fromARGB(255, 22, 21, 66)],
+                                      begin: Alignment.topLeft,
+                                      end: Alignment.bottomRight,
+                                    ),
+                                    borderRadius: BorderRadius.circular(8),
+                                  ),
+                                  child: ElevatedButton.icon(
+                                    onPressed: () {
+                                      //
+                                    },
+                                    icon: const Icon(
+                                      Icons.backup,
+                                      color: Colors.white,
+                                    ),
+                                    label: const Text(
+                                      "Export as CSV",
+                                      style: TextStyle(color: Colors.white),
+                                    ),
+                                    style: ElevatedButton.styleFrom(
+                                      backgroundColor: Colors.transparent,
+                                      shadowColor: Colors.transparent,
+                                      shape: RoundedRectangleBorder(
+                                        borderRadius: BorderRadius.circular(8),
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                            
+                            
                             ],
                           ),
                         ],
